5214767414b33fd50b49f1b3e57d5b894182851f
Andrey Nepomnyaschikh <And-dk@mail.ru>
2023-02-05 13:52:14 +0700

    seminar10

--- venv/handlers.py
+++ venv/handlers.py
@@ -2,23 +2,36 @@ from loader import dp, bot
 from aiogram import types
 from random import randint
 
-total = 150
+total = []
 p = 1
 
 @dp.message_handler(commands=['start'])
 async def mes_start(message: types.Message):
     await bot.send_message(message.from_user.id, f'Привет, {message.from_user.first_name}!\n'
                             f'Я хочу поиграть с тобой в игру...\n'
-                            f'На столе лежит {total} конфет.\n'
+                            f'На столе лежит 150 конфет.\n'
                             f'Возьми от 1 до 28 конфет так, что бы твой ход оказался последним.\n'
                             f'Помощь - /help')
+    my_game = [message.from_user.id, message.from_user.first_name, 150]
+    total.append(my_game)
 
 @dp.message_handler(commands=['set'])
 async def mes_set(message: types.Message):
     global total
     count = message.text.split()[1]
-    total = int(count)
-    await bot.send_message(message.from_user.id, f'Установил новое количество конфет в размере: {total}')
+    for duel in total:
+        if message.from_user.id == duel[0]:
+            duel[2] = int(count)
+            # total.append(my_game)
+            await bot.send_message(message.from_user.id, f'Установил новое количество конфет в размере: {duel[2]}')
+            print(total)
+            mes_all()
+        else:
+            my_game = [message.from_user.id, message.from_user.first_name, int(count)]
+            total.append(my_game)
+            await bot.send_message(message.from_user.id, f'Установил новое количество конфет в размере: {duel[2]}')
+            print(total)
+            mes_all()
 
 @dp.message_handler(commands=['help'])
 async def mes_help(message: types.Message):
@@ -29,31 +42,33 @@ async def mes_help(message: types.Message):
 async def mes_all(message: types.Message):
     global total
     global p
-    if message.text.isdigit():
-        while total > 28:
-            if int(message.text) > 0 and int(message.text) < 29:
-                if p == 1:
-                    total -= int(message.text)
-                    await bot.send_message(message.from_user.id, f'Конфет осталось {total}')
-                    p = 0
-                else:
-                    a = randint(1, 28)
-                    await bot.send_message(message.from_user.id, f'А я возьму {a}!')
-                    total -= a
-                    await bot.send_message(message.from_user.id, f'Конфет осталось {total}')
-                    p = 1
-                    mes_all()
+    for duel in total:
+        if message.from_user.id == duel[0]:
+            if message.text.isdigit():
+                while duel[2] > 28:
+                    if 0 < int(message.text) < 29:
+                        if p == 1:
+                            duel[2] -= int(message.text)
+                            await bot.send_message(message.from_user.id, f'Конфет осталось {duel[2]}')
+                            p = 0
+                        else:
+                            a = randint(1, 28)
+                            await bot.send_message(message.from_user.id, f'А я возьму {a}!')
+                            duel[2] -= a
+                            await bot.send_message(message.from_user.id, f'Конфет осталось {duel[2]}')
+                            p = 1
+                            mes_all()
+                    else:
+                        await bot.send_message(message.from_user.id, f'Введи количество от 1 до 28!')
+                        mes_all()
             else:
-                await bot.send_message(message.from_user.id, f'Введи количество от 1 до 28!')
+                await bot.send_message(message.from_user.id, f'Введи число!')
                 mes_all()
-    else:
-        await bot.send_message(message.from_user.id, f'Введи число!')
-        mes_all()
-    if p == 1:
-        await bot.send_message(message.from_user.id, f'Ты выиграл, молодец! Давай заново)')
-        total = 150
-        p = 1
-    else:
-        await bot.send_message(message.from_user.id, f'Выиграл я! Го еще)')
-        total = 150
-        p = 1
+        if p == 1:
+            await bot.send_message(message.from_user.id, f'Ты выиграл, молодец! Давай заново)')
+            duel[2] = 150
+            p = 1
+        else:
+            await bot.send_message(message.from_user.id, f'Выиграл я! Го еще)')
+            duel[2] = 150
+            p = 1
